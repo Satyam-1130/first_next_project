8296d55525abc59094d7aa764b439ced112e6c32
satyam-1130 <satyam.1130@zenmonk.tech>
2023-12-08 10:43:35 +0530

    added SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 export default function RootLayout({
   children,
@@ -8,6 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <SpeedInsights/>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
